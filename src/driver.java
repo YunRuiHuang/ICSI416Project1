4eb63c905a714e70fe0ce32eeb86a55eec9bd465
YunRuiHuang <yunruihuang@gmail.com>
2022-03-24 11:17:43 -0400

    test server

--- src/driver.java
+++ src/driver.java
@@ -1,6 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class driver {
-    public static void main(String[] args){
-        new testing();
+    public static void main(String[] args) throws IOException {
+//        new testing();
+        client_tcp client = new client_tcp();
+        client.startConnection("127.0.0.1", 6666);
+        String response = client.sendMessage("hello server");
+        client.sendMessage("stop");
+
+        Scanner scanner = new Scanner(System.in);
+        client.sendMessage(scanner.nextLine());
+//        assertEquals("hello client", response);
+
     }
 
 }
