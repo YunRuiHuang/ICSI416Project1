3717757c0a5dc52b283c83abef93732e9fa31323
YunRuiHuang <yunruihuang@gmail.com>
2022-03-30 21:32:35 -0400

    udp

--- src/client_udp.java
+++ src/client_udp.java
@@ -1,9 +1,30 @@
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class client_udp {
 
     public static void main(String[] args) throws IOException {
+        InetAddress address = InetAddress.getByName("127.0.0.1");
+        int port = 11111;
+        byte[] sendPacket = "hello ".getBytes();
+        DatagramPacket reportPacket = new DatagramPacket(sendPacket, sendPacket.length, address, port);
+        DatagramSocket socket = new DatagramSocket();
+        socket.send(reportPacket);
 
+        byte[] packSize = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(packSize, packSize.length);
+        socket.receive(packet);
+        String line = new String(packSize, 0, packSize.length);
+
+        String[] msg = line.split(" ");
+        line = "";
+        for(int i = 0; i < (msg.length-1); i++){
+            line = line + msg[i] + " ";
+        }
+        System.out.println("massage : " + line);
+        socket.close();
     }
 
 }
--- src/server_udp.java
+++ src/server_udp.java
@@ -1,8 +1,34 @@
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class server_udp {
 
     public static void main(String[] args) throws IOException {
+
+        DatagramSocket socket = new DatagramSocket(11111);
+        byte[] packSize = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(packSize, packSize.length);
+
+        System.out.println("wait for connect");
+        socket.receive(packet);
+        String line = new String(packSize, 0, packSize.length);
+        String[] msg = line.split(" ");
+        line = "";
+        for(int i = 0; i < (msg.length-1); i++){
+            line = line + msg[i] + " ";
+        }
+        System.out.println("massage : " + line);
+
+        InetAddress address = packet.getAddress();
+        int port = packet.getPort();
+        byte[] report = line.getBytes();
+        DatagramPacket reportPacket = new DatagramPacket(report, report.length, address, port);
+        socket.send(reportPacket);
+        socket.close();
+
+
 
     }
 }
